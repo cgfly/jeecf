718dab90bc634f554468f23ad4dd88e242876b1f
jianym <3502168163@qq.com>
2019-01-24 16:22:06 +0800

    优化 使用maven中央仓库

--- jeecf-module-manager/src/main/java/org/jeecf/manager/gen/builder/TableBuilder.java
+++ jeecf-module-manager/src/main/java/org/jeecf/manager/gen/builder/TableBuilder.java
@@ -24,122 +24,124 @@ import org.jeecf.manager.module.template.service.GenTableColumnService;
 import org.jeecf.manager.module.template.service.GenTableService;
 import org.jeecf.manager.proxy.TargetTableProxy;
 import org.springframework.beans.BeanUtils;
+
 /**
- * 语言构建类
+ * 表构建类
+ * 
  * @author jianyiming
  *
  */
 public class TableBuilder {
-	
-	private static GenTableService genTableService = SpringContextUtils.getBean("genTableService",
-			GenTableService.class);
-	
-	private static GenTableFacade genTableFacade = SpringContextUtils.getBean("genTableFacade",
-			GenTableFacade.class);
 
-	private static GenTableColumnService genTableColumnService = SpringContextUtils.getBean("genTableColumnService",
-			GenTableColumnService.class);
-	
-	private static TargetTableProxy targetTableProxy = SpringContextUtils.getBean("targetTableProxy",TargetTableProxy.class);
+    private static GenTableService genTableService = SpringContextUtils.getBean("genTableService", GenTableService.class);
 
-	private BaseTable baseTable;
-	
-	public BaseTable build(String tableName) {
-		GenTableQuery queryGenTable = new GenTableQuery();
-		queryGenTable.setName(tableName);
-		List<GenTableResult> genTableList = genTableService.findListByAuth(new GenTablePO(queryGenTable)).getData();
-		GenTableResult genTable = null;
-		if (CollectionUtils.isNotEmpty(genTableList)) {
-		    genTable = genTableList.get(0);
-			GenTableColumnQuery queryTableColumn = new GenTableColumnQuery();
-			queryTableColumn.setGenTable(genTable);
-			Response<List<GenTableColumnResult>> tableColumnRes = genTableColumnService
-					.findList(new GenTableColumnPO(queryTableColumn));
-			List<GenTableColumnResult> tableColumnList = tableColumnRes.getData();
-			if (CollectionUtils.isNotEmpty(tableColumnList)) {
-				genTable.setGenTableColumns(tableColumnList);
-			}
-		}
-		BaseTable baseTable = new BaseTable();
-		BeanUtils.copyProperties(genTable, baseTable);
-		List<GenTableColumnResult> genTableColumnResultList = genTable.getGenTableColumns();
-	    baseTable.setGenTableColumns(buildCommonTableColumn(genTableColumnResultList));
-	    
-		baseTable.setParent(getParent(genTable.getParentTableId()));
-		baseTable.setChildList(getChilds(genTable.getId()));
-		this.baseTable = baseTable;
-		return baseTable;
-	}
+    private static GenTableFacade genTableFacade = SpringContextUtils.getBean("genTableFacade", GenTableFacade.class);
 
-	/**
-	 * 获取表数据 
-	 * @param whereEntitys
-	 * @return
-	 */
-	public  String getData(List<WhereEntity> whereEntitys) {
-		if(this.baseTable != null) {
-			SelectTable selectTable = SelectTable.Builder.build(this.baseTable.getClassName(), this.baseTable.getName());
-			List<SelectTableColumn> columnList = new ArrayList<>();
-			List<BaseTableColumn> tableColumnList = this.baseTable.getGenTableColumns();
-			tableColumnList.forEach(tableColumn->{
-				columnList.add(SelectTableColumn.Builder.build(tableColumn.getField(), tableColumn.getName()));
-			});
-			selectTable.setWhereEntitys(whereEntitys);
-			selectTable.setSelectTableColumns(columnList);
-			return targetTableProxy.selectTable(selectTable).getData();
-		}
-		return null;
-	}
-	
-	/**
-	 * 获取子表数据
-	 * @param id
-	 * @return
-	 */
-	private List<CommonTable> getChilds(String id) {
-		List<GenTableResult> tableResultList = TableBuilder.genTableFacade.findChildTables(id).getData();
-		List<CommonTable> childTables = new ArrayList<>();
-		if(CollectionUtils.isNotEmpty(tableResultList)) {
-			tableResultList.forEach(tableResult -> {
-				CommonTable childTable = new CommonTable();
-				BeanUtils.copyProperties(tableResult, childTable);
-				List<GenTableColumnResult> genTableColumnResultList = tableResult.getGenTableColumns();
-				childTable.setGenTableColumns(buildCommonTableColumn(genTableColumnResultList));
-				childTables.add(childTable);
-			});
-		}
-		return childTables;
-	}
-	
-	/**
-	 * 获取父表数据
-	 * @param id
-	 * @return
-	 */
-	private CommonTable getParent(String id) {
-		GenTable parentTable = TableBuilder.genTableFacade.findParentTable(id).getData();
-		CommonTable parentCommonTable = new CommonTable();
-		if(parentTable != null) {
-			BeanUtils.copyProperties(parentTable, parentCommonTable);
-			List<GenTableColumnResult> genTableColumnResultList = parentTable.getGenTableColumns();
-			parentCommonTable.setGenTableColumns(buildCommonTableColumn(genTableColumnResultList));
-		}
-		return parentCommonTable;
-	}
-	
-	/**
-	 * 构建 commonTableColumn
-	 * @param genTableColumnResultList
-	 * @return
-	 */
-	private List<BaseTableColumn> buildCommonTableColumn(List<GenTableColumnResult> genTableColumnResultList) {
-		List<BaseTableColumn> baseTableColumns = new ArrayList<>();
-		genTableColumnResultList.forEach(tableColumnResult -> {
-			BaseTableColumn baseTableColumn = new BaseTableColumn();
-			BeanUtils.copyProperties(tableColumnResult, baseTableColumn);
-			baseTableColumns.add(baseTableColumn);
-		});
-		return baseTableColumns;
-	}
+    private static GenTableColumnService genTableColumnService = SpringContextUtils.getBean("genTableColumnService", GenTableColumnService.class);
+
+    private static TargetTableProxy targetTableProxy = SpringContextUtils.getBean("targetTableProxy", TargetTableProxy.class);
+
+    private BaseTable baseTable;
+
+    public BaseTable build(String tableName) {
+        GenTableQuery queryGenTable = new GenTableQuery();
+        queryGenTable.setName(tableName);
+        List<GenTableResult> genTableList = genTableService.findListByAuth(new GenTablePO(queryGenTable)).getData();
+        GenTableResult genTable = null;
+        if (CollectionUtils.isNotEmpty(genTableList)) {
+            genTable = genTableList.get(0);
+            GenTableColumnQuery queryTableColumn = new GenTableColumnQuery();
+            queryTableColumn.setGenTable(genTable);
+            Response<List<GenTableColumnResult>> tableColumnRes = genTableColumnService.findList(new GenTableColumnPO(queryTableColumn));
+            List<GenTableColumnResult> tableColumnList = tableColumnRes.getData();
+            if (CollectionUtils.isNotEmpty(tableColumnList)) {
+                genTable.setGenTableColumns(tableColumnList);
+            }
+        }
+        BaseTable baseTable = new BaseTable();
+        BeanUtils.copyProperties(genTable, baseTable);
+        List<GenTableColumnResult> genTableColumnResultList = genTable.getGenTableColumns();
+        baseTable.setGenTableColumns(buildCommonTableColumn(genTableColumnResultList));
+
+        baseTable.setParent(getParent(genTable.getParentTableId()));
+        baseTable.setChildList(getChilds(genTable.getId()));
+        this.baseTable = baseTable;
+        return baseTable;
+    }
+
+    /**
+     * 获取表数据
+     * 
+     * @param whereEntitys
+     * @return
+     */
+    public String getData(List<WhereEntity> whereEntitys) {
+        if (this.baseTable != null) {
+            SelectTable selectTable = SelectTable.Builder.build(this.baseTable.getClassName(), this.baseTable.getName());
+            List<SelectTableColumn> columnList = new ArrayList<>();
+            List<BaseTableColumn> tableColumnList = this.baseTable.getGenTableColumns();
+            tableColumnList.forEach(tableColumn -> {
+                columnList.add(SelectTableColumn.Builder.build(tableColumn.getField(), tableColumn.getName()));
+            });
+            selectTable.setWhereEntitys(whereEntitys);
+            selectTable.setSelectTableColumns(columnList);
+            return targetTableProxy.selectTable(selectTable).getData();
+        }
+        return null;
+    }
+
+    /**
+     * 获取子表数据
+     * 
+     * @param id
+     * @return
+     */
+    private List<CommonTable> getChilds(String id) {
+        List<GenTableResult> tableResultList = TableBuilder.genTableFacade.findChildTables(id).getData();
+        List<CommonTable> childTables = new ArrayList<>();
+        if (CollectionUtils.isNotEmpty(tableResultList)) {
+            tableResultList.forEach(tableResult -> {
+                CommonTable childTable = new CommonTable();
+                BeanUtils.copyProperties(tableResult, childTable);
+                List<GenTableColumnResult> genTableColumnResultList = tableResult.getGenTableColumns();
+                childTable.setGenTableColumns(buildCommonTableColumn(genTableColumnResultList));
+                childTables.add(childTable);
+            });
+        }
+        return childTables;
+    }
+
+    /**
+     * 获取父表数据
+     * 
+     * @param id
+     * @return
+     */
+    private CommonTable getParent(String id) {
+        GenTable parentTable = TableBuilder.genTableFacade.findParentTable(id).getData();
+        CommonTable parentCommonTable = new CommonTable();
+        if (parentTable != null) {
+            BeanUtils.copyProperties(parentTable, parentCommonTable);
+            List<GenTableColumnResult> genTableColumnResultList = parentTable.getGenTableColumns();
+            parentCommonTable.setGenTableColumns(buildCommonTableColumn(genTableColumnResultList));
+        }
+        return parentCommonTable;
+    }
+
+    /**
+     * 构建 commonTableColumn
+     * 
+     * @param genTableColumnResultList
+     * @return
+     */
+    private List<BaseTableColumn> buildCommonTableColumn(List<GenTableColumnResult> genTableColumnResultList) {
+        List<BaseTableColumn> baseTableColumns = new ArrayList<>();
+        genTableColumnResultList.forEach(tableColumnResult -> {
+            BaseTableColumn baseTableColumn = new BaseTableColumn();
+            BeanUtils.copyProperties(tableColumnResult, baseTableColumn);
+            baseTableColumns.add(baseTableColumn);
+        });
+        return baseTableColumns;
+    }
 
 }
